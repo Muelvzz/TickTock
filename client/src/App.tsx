d4b682cb58c90efa35c6f5caf3be0b133aa592e1
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-09-20 14:19:03 +0800

    setup: files and folders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,20 @@
 import "./App.css"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
+import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />}/>
+          <Route path="/home" element={<Home />}/>
+          <Route path="*" element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
-
-export default App
